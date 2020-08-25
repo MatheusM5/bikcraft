ca5e38471f97a50e0ad7e36f5b7483c1d323d452
Matheus <matheus.medeiros55@hotmail.com>
2020-08-25 20:58:16 -0300

    suas alteracoes

---
+++ js/script.js
@@ -0,0 +1,15 @@
+if (window.SimpleSlide) {
+  new SimpleSlide({
+    slide: "quote",
+    Time: 5000,
+  });
+
+  new SimpleSlide({
+    slide: "portfolio",
+    Time: 5000,
+    nav: true,
+  });
+}
+if (window.SimpleAnime) {
+  new SimpleAnime();
+}
